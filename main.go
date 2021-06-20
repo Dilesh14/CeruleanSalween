6e8f5e409c0a668f6df51a6e740c89590cd1f360
Dilesh <drj855@mun.ca>
2021-06-20 00:19:08 -0230

    just a few message

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "welcome to the homepage!")
+	fmt.Fprintf(w, "welcome to the homepage! \n This website will change over the course of time as i get familiar with web programming with go. \n Oh Yeah! the backend of this website is written using go.")
 }
 
 func handleRequest() {
