79107ed8b0ad444b460a6ecdd29aa550eb48500a
Dilesh <drj855@mun.ca>
2021-06-20 00:07:46 -0230

    changed the port to get from env

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -11,8 +12,13 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequest() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func main() {
