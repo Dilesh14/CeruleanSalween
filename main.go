0ec3524121627748286052b95bd6f816dbfb828a
Dilesh <drj855@mun.ca>
2021-06-21 15:37:34 -0230

    end point for displaying all the db connections

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 
+	dataadapter "github.com/dilesh14/CeruleanSalween/db"
 	"github.com/joho/godotenv"
 )
 
@@ -13,18 +14,30 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "welcome to the homepage! \n This website will change over the course of time as i get familiar with web programming with go. \n Oh Yeah! the backend of this website is written using go.")
 }
 
-func handleRequest() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("$PORT must be set")
+func getDb(w http.ResponseWriter, r *http.Request) {
+	uri := os.Getenv("mongoDbConnectionString")
+	if uri == "" {
+		log.Fatalf("db uri found not")
 	}
 
+	databases := dataadapter.LoadAllDatabases(uri)
+	fmt.Fprintf(w, "databases: %v\n", databases)
+}
+
+func handleRequest() {
 	envVarError := godotenv.Load()
 	if envVarError != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/db", getDb)
+
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
