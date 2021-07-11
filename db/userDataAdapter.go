e545038bb5a9611ea64ff782dac72906e6e04c9e
Dilesh <drj855@mun.ca>
2021-07-11 13:08:05 -0230

    handled creating user and hashing password

---
+++ db/userDataAdapter.go
@@ -0,0 +1,62 @@
+package dataadapter
+
+import (
+	"context"
+	"log"
+	"time"
+
+	services "github.com/dilesh14/CeruleanSalween/services"
+	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type User struct {
+	ExternalId   string
+	UserName     string
+	EmailAddress string
+	PhoneNumber  string
+	Password     string
+}
+
+var connectionString string
+
+func SetDbConnectionString(cs string) {
+	connectionString = cs
+}
+
+func CreateUser(user User) {
+	user.ExternalId = uuid.NewString()
+
+	passHash, err := services.HashPassword(user.Password)
+
+	if err != nil {
+		log.Fatal("error in hashing password")
+	}
+
+	user.Password = passHash
+
+	//connection to db
+	clientOptions := options.Client().ApplyURI(connectionString)
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal("failed connection to client")
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 15*time.Second)
+	coll := client.Database("passManager").Collection("Users")
+	result, inserErr := coll.InsertOne(ctx, user)
+	if inserErr != nil {
+		log.Fatal(inserErr)
+	}
+	log.Println("inserted new user, new userId: ", result.InsertedID)
+}
+
+func DeleteUser(id string) {
+
+}
+
+func AllUserSite(userId string) {
+
+}
