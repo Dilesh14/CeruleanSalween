a0aa61985f23ab3797f57d31fa4431b89f52c580
Dilesh <drj855@mun.ca>
2021-07-13 22:43:57 -0230

    handling req for creating user

--- router/router.go
+++ router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,13 @@ import (
 
 	dataadapter "github.com/dilesh14/CeruleanSalween/db"
 )
+
+type CUserReq struct {
+	Username    string `json:"uname"`
+	PhoneNumber string `json:"phone"`
+	Email       string `json:"email"`
+	Password    string `json:"password"`
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello to the homepage! \n This website will change over the course of time as i get familiar with web programming with go. \n Oh Yeah! the backend of this website is written using go.")
@@ -27,17 +35,33 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	switch r.Method {
 	case "POST":
+		hContentType := r.Header.Get("Content-Type")
+		if hContentType != "application/json" {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			return
+		}
+		var u CUserReq
+
+		decoder := json.NewDecoder(r.Body)
+		decoder.DisallowUnknownFields()
+		err := decoder.Decode(&u)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
 		user := dataadapter.User{
-			UserName:     "buklau",
-			PhoneNumber:  "123131321",
-			EmailAddress: "test.go.go",
-			Password:     "this is yo ma",
+			UserName:     u.Username,
+			PhoneNumber:  u.PhoneNumber,
+			EmailAddress: u.Email,
+			Password:     u.Password,
 		}
 		uri := os.Getenv("mongoDbConnectionString")
 		dataadapter.SetDbConnectionString(uri)
 		dataadapter.CreateUser(user)
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message": "get called"}`))
+		w.Write([]byte(`{"message": "user created"}`))
 	default:
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte(`{"message": "not found"}`))
